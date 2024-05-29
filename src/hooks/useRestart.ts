841a7e0d5f03a91bf7fda47f2c4af6062bf59998
mobelite-fkchaou <fkchaou@mobelite.fr>
2024-05-29 10:23:36 +0100

    add obejct to the game

--- src/hooks/useRestart.ts
+++ src/hooks/useRestart.ts
@@ -1,6 +1,11 @@
 import Matter from "matter-js";
 import { Dimensions } from "react-native";
-import Bird from "../components/brid";
+import Ball from "../components/ball";
+import Obstacle from "../components/obstacle";
+
+import Floor from "../components/floor";
+import { COLORS } from "../utils/colors";
+import { getPipeSizePosPair } from "./useRandom";
 
 const windowHeight = Dimensions.get("window").height;
 const windowWidth = Dimensions.get("window").width;
@@ -12,9 +17,58 @@ export default (restart) => {
 
   world.gravity.y = 0.4;
 
+  const pipeSizePosA = getPipeSizePosPair();
+  const pipeSizePosB = getPipeSizePosPair(windowWidth * -3);
   return {
     physics: { engine, world },
 
-    Bird: Bird(world, "green", { x: 50, y: 300 }, { height: 40, width: 40 }),
+    Ball: Ball(
+      world,
+      "green",
+      { x: windowWidth / 2, y: 300 },
+      { height: 40, width: 40 }
+    ),
+    ObstacleTop1: Obstacle(
+      world,
+      "ObstacleTop1",
+      COLORS.Red,
+      pipeSizePosA.pipeTop.pos,
+      pipeSizePosA.pipeTop.size
+    ),
+    ObstacleBottom1: Obstacle(
+      world,
+      "ObstacleBottom1",
+      COLORS.Blue,
+      pipeSizePosA.pipeBottom.pos,
+      pipeSizePosA.pipeBottom.size
+    ),
+
+    ObstacleTop2: Obstacle(
+      world,
+      "ObstacleTop2",
+      "red",
+      pipeSizePosB.pipeTop.pos,
+      pipeSizePosB.pipeTop.size
+    ),
+    ObstacleBottom2: Obstacle(
+      world,
+      "ObstacleBottom2",
+      "blue",
+      pipeSizePosB.pipeBottom.pos,
+      pipeSizePosB.pipeBottom.size
+    ),
+    Floor: Floor(
+      world,
+      COLORS.Green,
+      { x: windowWidth / 2, y: windowHeight },
+      { height: 50, width: windowWidth }
+    ),
+
+    FloorTop: Floor(
+      world,
+      COLORS.Green,
+      { x: windowWidth / 2, y: 0 },
+      { height: 50, width: windowWidth }
+    ),
   };
 };
--- src/utils/globalStyles.ts
+++ src/utils/globalStyles.ts
@@ -1,5 +1,8 @@
+import { COLORS } from "./colors";
+
 export const GlobalStyles = {
   container: {
     flex: 1,
+    backgroundColor: COLORS.Default,
   },
 };
